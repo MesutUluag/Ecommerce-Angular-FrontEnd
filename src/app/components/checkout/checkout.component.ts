54511c8bee7dac9a1437d8fb65174f1326a6ca41
developer <punk7119@gmail.com>
2020-12-02 17:08:38 +0300

    add form-validation for firstname lastname and email address

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {CheckoutServiceService} from '../../services/checkout-service.service';
 import {Country} from '../../common/country';
 import {State} from '../../common/state';
@@ -26,9 +26,9 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl('', [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -77,7 +77,14 @@ export class CheckoutComponent implements OnInit {
   onSubmit() {
     console.log('Handling Submit Button');
     console.log(this.checkoutFormGroup.get('customer').value);
+    if (this.checkoutFormGroup.invalid){
+      this.checkoutFormGroup.markAllAsTouched();
+    }
   }
+
+  get firstName(){ return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName(){ return this.checkoutFormGroup.get('customer.lastName'); }
+  get email(){ return this.checkoutFormGroup.get('customer.email'); }
 
   copyShippingAddressToBillingAddress(event) {
     if (event.target.checked) {
