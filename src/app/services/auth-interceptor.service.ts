a284a92f957f9d02fa2a0cdcd3736814c022c04e
mesut uluag <punk7119@gmail.com>
2022-02-17 11:49:00 +0300

    secure orders history endpoint - reach using auth token

---
+++ src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {from, Observable} from 'rxjs';
+import {OktaAuthService} from '@okta/okta-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
+
+  constructor(private oktaAuth: OktaAuthService) {
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return from(this.handleAccess(request, next));
+  }
+
+  private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+    // add secured endpoints
+    const securedEndpoints = ['http://localhost:8080/api/orders'];
+
+    if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+      const accessToken = await this.oktaAuth.getAccessToken();
+
+      // add new header with access token
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + accessToken
+        }
+      });
+    }
+
+    return next.handle(request).toPromise();
+  }
+
+}
