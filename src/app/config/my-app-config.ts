589e62db2103794f433790e6cef7c43d04fd59fe
mesut uluag <punk7119@gmail.com>
2022-02-17 15:27:38 +0300

    https update - login part

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa3fgfxsf3nXV67O5d7',
         issuer: 'https://dev-19547582.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 
