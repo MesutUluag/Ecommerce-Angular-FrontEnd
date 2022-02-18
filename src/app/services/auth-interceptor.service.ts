7d687f28c1e2884eae199b8a5ae84e783bb4ad64
mesut uluag <punk7119@gmail.com>
2022-02-18 10:48:05 +0300

    define base url at environment variable

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {from, Observable} from 'rxjs';
 import {OktaAuthService} from '@okta/okta-angular';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -16,8 +17,10 @@ export class AuthInterceptorService implements HttpInterceptor {
   }
 
   private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+
+    const endPoint = environment.apiUrl + '/orders';
     // add secured endpoints
-    const securedEndpoints = ['http://localhost:8080/api/orders'];
+    const securedEndpoints = [endPoint];
 
     if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
       const accessToken = await this.oktaAuth.getAccessToken();
